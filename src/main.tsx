060bac4e654636e591d530ace62aaa5a7107f58f
necho1122 <nelsonubac90@gmail.com>
2024-06-11 18:47:30 -0300

    add updates

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,26 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
-import Quote from "./Quote.tsx";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import Quote from './Quote';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-const rootElement = document.getElementById("root");
+const rootElement = document.getElementById('root');
 if (rootElement) {
 	ReactDOM.createRoot(rootElement).render(
 		<React.StrictMode>
-			
 			<Router>
 				<Routes>
-          			<Route path="/" element={<App />} />
-					<Route path="/quote" element={<Quote />} />
+					<Route
+						path='/'
+						element={<App />}
+					/>
+					<Route
+						path='/quote'
+						element={<Quote />}
+					/>
 				</Routes>
 			</Router>
-		</React.StrictMode>,
+		</React.StrictMode>
 	);
 }
