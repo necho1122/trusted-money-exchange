5b627455f9cb1690044e0fcc2b9f79bd75912d0b
necho1122 <nelsonubac90@gmail.com>
2024-06-11 16:53:12 -0300

    ...

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'build', // Especifica la carpeta de salida como 'build'
-  },
+	plugins: [react()],
+	build: {
+		outDir: 'dist',
+	},
 });
-
